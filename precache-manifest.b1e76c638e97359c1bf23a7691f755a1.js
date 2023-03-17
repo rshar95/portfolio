38e2bcecd7f4aa0674791b66c0da4ebf69368b9a
Richa Sharma <annie@richas-air.mynetworksettings.com>
2023-03-17 00:46:57 -0400

    Updates

---
+++ precache-manifest.b1e76c638e97359c1bf23a7691f755a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60306a6ad7742e08648cf61d8db0e99d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a0f013ab64a60b29a06",
+    "url": "/static/css/2.ad5ce37f.chunk.css"
+  },
+  {
+    "revision": "d61233a1b2aa04eec5e5",
+    "url": "/static/css/main.8e357cb2.chunk.css"
+  },
+  {
+    "revision": "1a0f013ab64a60b29a06",
+    "url": "/static/js/2.fba63226.chunk.js"
+  },
+  {
+    "revision": "b12aaea9c7d723083f2c844fa56b8dc2",
+    "url": "/static/js/2.fba63226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d61233a1b2aa04eec5e5",
+    "url": "/static/js/main.d834c3a9.chunk.js"
+  },
+  {
+    "revision": "7aac4c2cfe4d9b3f292a",
+    "url": "/static/js/runtime-main.ec4bb848.js"
+  }
+]);
